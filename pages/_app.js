1de44acf4e7e5621d37025d8012d742864e1ee8b
Phạm Phú Quý Đôn <donezombie@gmail.com>
2019-10-09 17:56:13 +0700

    update new

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
-import App from 'next/app'
-import React from 'react'
-import { Provider } from 'react-redux'
-import withRedux from 'next-redux-wrapper'
-import withReduxSaga from 'next-redux-saga'
-
+import App from 'next/app';
+import React from 'react';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import Head from 'next/head';
+import Link from 'next/link';
+import { Provider } from 'react-redux';
+import withRedux from 'next-redux-wrapper';
+import withReduxSaga from 'next-redux-saga';
 import createStore from '../store'
+
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 class MyApp extends App {
   static async getInitialProps ({ Component, ctx }) {
@@ -21,6 +31,10 @@ class MyApp extends App {
     const { Component, pageProps, store } = this.props
     return (
       <Provider store={store}>
+        <Head>
+          <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
+        </Head>
+
         <Component {...pageProps} />
       </Provider>
     )
