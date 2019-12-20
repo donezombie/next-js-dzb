f70ffab9cdc22d495d4f6f5ab4cf970c4b28dab9
Phạm Phú Quý Đôn <donezombie@gmail.com>
2019-12-20 14:39:59 +0700

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
-import App from 'next/app';
 import React from 'react';
+import App from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import Head from 'next/head';
-import Link from 'next/link';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 import withReduxSaga from 'next-redux-saga';
@@ -18,13 +17,12 @@ Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 class MyApp extends App {
-  static async getInitialProps ({ Component, ctx }) {
-    let pageProps = {}
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps({ ctx })
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
     }
-
-    return { pageProps }
   }
 
   render () {
@@ -33,7 +31,7 @@ class MyApp extends App {
       <Provider store={store}>
         <Layout>
           <Head>
-            <link rel='stylesheet' type='text/css' href='/static/nprogress.css' />
+            <link rel="shortcut icon" type="image/png" href="static/favicon.ico"/>
           </Head>
           <Component {...pageProps} />
         </Layout>
